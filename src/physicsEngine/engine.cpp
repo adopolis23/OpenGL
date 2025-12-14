e56f2a1192628de7aa4bd0f1c8bd548aa8617af0
adopolis23 <brandonw406@gmail.com>
2025-12-13 22:56:02 -0500

    started working on calculating density

--- src/physicsEngine/engine.cpp
+++ src/physicsEngine/engine.cpp
@@ -29,6 +29,16 @@ void Engine::CalculateDensity(const Scene& scene)
     // reset d-field to all 0s
     std::fill(densityField.density.begin(), densityField.density.end(), 0.0f);
 
-    densityField.density[5000] += 10.0f;
+    for (auto obj : scene.objects)
+    {
+        int x_bin = (obj->position.x * (densityField.width / 2)) + (densityField.width / 2);
+        
+        for (int y = 0; y < 100; y++)
+        {
+            densityField.density[(y*100) + x_bin] += 1.0;
+        }
+    }
+
+
 
 }
